13043d3e0b4fff0d34819713c67b41c98ab8e6ba
Nahom Getnet <74378013+Nahom-Getnet@users.noreply.github.com>
2026-07-12 03:47:11 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import About from "../components/sections/About";
 import CoffeePortfolio from "../components/sections/CoffeePortfolio";
 import Services from "../components/sections/Services";
 import Process from "../components/sections/Process";
-import WhyChooseUs from "../components/sections/WhyChooseUs";
+import WhychooseUs from "../components/sections/WhyChooseUs";
 import ImageBanner from "../components/sections/ImageBanner";
 import FAQ from "../components/sections/FAQ";
 import Contact from "../components/sections/Contact";
